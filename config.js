f3fa3a4ee675a8a0b215efd9f26dc380805c90af
Zachary Scott <zachary.douglas.scott@gmail.com>
2019-03-04 12:57:16 -0500

    troubleshooting jenkins env

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
+console.log(process.env.frm_un);
+console.log(process.env.frm_ps);
 module.exports = {
   un: process.env.frm_un,
   ps: process.env.frm_ps,
